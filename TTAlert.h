bda5acf2c755d3519b392e19d35866ac7b8dea8a
ap4y <lod@pisem.net>
2012-03-29 11:51:38 +0400

    alert helper added

---
+++ TTAlert.h
@@ -0,0 +1,16 @@
+//
+//  TTAlert.h
+//  10tracks
+//
+//  Created by Arthur Evstifeev on 11/18/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TTAlert : NSObject
+
++ (void)composeAlertViewWithTitle:(NSString*)title 
+                       andMessage:(NSString*)message;
+
+@end
---
+++ TTAlert.m
@@ -0,0 +1,20 @@
+//
+//  TTAlert.m
+//  10tracks
+//
+//  Created by Arthur Evstifeev on 11/18/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "TTAlert.h"
+
+@implementation TTAlert
+
++ (void)composeAlertViewWithTitle:(NSString*)title 
+                       andMessage:(NSString*)message
+{
+    UIAlertView* alert = [[UIAlertView alloc] initWithTitle:title message:message delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil, nil];
+    [alert performSelectorOnMainThread:@selector(show) withObject:nil waitUntilDone:NO];
+}
+
+@end
