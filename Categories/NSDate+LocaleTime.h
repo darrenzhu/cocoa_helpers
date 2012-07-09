6d2261413187a8e15ba842808526fee7b23dbbe6
ap4y <lod@pisem.net>
2012-07-09 23:46:22 +0400

    NSDate category added for locale time conversion

---
+++ Categories/NSDate+LocaleTime.h
@@ -0,0 +1,13 @@
+//
+//  NSDate+LocaleTime.h
+//  Pods
+//
+//  Created by Arthur Evstifeev on 7/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDate (LocaleTime)
+
+@end
---
+++ Categories/NSDate+LocaleTime.m
@@ -0,0 +1,13 @@
+//
+//  NSDate+LocaleTime.m
+//  Pods
+//
+//  Created by Arthur Evstifeev on 7/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "NSDate+LocaleTime.h"
+
+@implementation NSDate (LocaleTime)
+
+@end
