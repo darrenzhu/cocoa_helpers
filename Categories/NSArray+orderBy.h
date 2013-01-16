e5d282011ed9e25edea3436ae352307cb8255a0f
ap4y <lod@pisem.net>
2013-01-16 19:54:20 +1300

    Revert "old array helper removed"
    This reverts commit 7b8357f96926c0fc909cd107ff2938929d94ac25.

---
+++ Categories/NSArray+orderBy.h
@@ -0,0 +1,17 @@
+//
+//  NSArray+NSArray_orderBy.h
+//  cska
+//
+//  Created by Arthur Evstifeev on 3/29/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSArray (orderBy)
+
+- (NSArray*)orderBy:(NSString*)firstSortingParam, ... NS_REQUIRES_NIL_TERMINATION;
+- (NSArray*)orderByDescriptors:(NSSortDescriptor*)firstDescriptor, ... NS_REQUIRES_NIL_TERMINATION;
+- (id)first;
+
+@end
