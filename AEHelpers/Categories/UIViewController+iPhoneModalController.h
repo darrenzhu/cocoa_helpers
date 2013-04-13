d8d8fb051b2d1e9948a9e1f22bed41b2fd303713
ap4y <lod@pisem.net>
2013-04-13 21:15:49 +1200

    header for modal controller helper updated

--- AEHelpers/Categories/UIViewController+iPhoneModalController.h
+++ AEHelpers/Categories/UIViewController+iPhoneModalController.h
@@ -36,10 +36,26 @@
 - (void)presentInIphoneModalViewController:(UIViewController *)controller;
 
 /**
+ Adds `controller` as a child view controller and presents it with animation in current view controller.
+ 
+ @param controller A controller to present.
+ @param completion A block that will be invoked upon completion.
+ */
+- (void)presentInIphoneModalViewController:(UIViewController *)controller completion:(void (^)())completion;
+    
+/**
  Dismisses `controller` from the screen, and removes it from child controllers.
  
  @param controller A controller to dismiss.
  */
 - (void)dismissIphoneModalViewController:(UIViewController *)controller;
+
+/**
+ Dismisses `controller` from the screen, and removes it from child controllers.
+ 
+ @param controller A controller to dismiss.
+ @param completion A block that will be invoked upon completion.
+ */
+- (void)dismissIphoneModalViewController:(UIViewController *)controller completion:(void (^)())completion;
 
 @end
